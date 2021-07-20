df66208739cf02283240d7234b959f4a25072549
loripepper <82188849+loripepper@users.noreply.github.com>
2021-07-19 18:36:03 -0700

    Updated with gender purchases

---
+++ HeroesOfPymoli_starter.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Note
+# * Instructions have been included for each segment. You do not have to follow them exactly, but they are included to help you think through the steps.
+
+# In[53]:
+
+
+# Dependencies and Setup
+import pandas as pd
+
+# File to Load (Remember to Change These)
+purchase_data = "./purchase_data.csv"
+
+# Read Purchasing File and store into Pandas data frame
+purchases_df = pd.read_csv(purchase_data)
+purchases_df
+
+
+# In[54]:
+
+
+purchases_df.dtypes
+
+
+# In[55]:
+
+
+purchases_df.describe()
+
+
+# ## Player Count
+
+# * Display the total number of players
+# 
+
+# In[73]:
+
+
+player_count = len(purchases_df["SN"].value_counts())
+print("Player Count", player_count)
+
+
+# ## Purchasing Analysis (Total)
+
+# * Run basic calculations to obtain number of unique items, average price, etc.
+# 
+# 
+# * Create a summary data frame to hold the results
+# 
+# 
+# * Optional: give the displayed data cleaner formatting
+# 
+# 
+# * Display the summary data frame
+# 
+
+# In[74]:
+
+
+# # unique_items
+# unique_items = len(purchases_df["Item ID"].value_counts())
+
+# print(unique_items)
+
+# average_price and limit to two decimal places
+average_price = purchases_df["Price"].mean()
+
+print("Average Price", average_price)
+
+# number_of_purchases
+purchase_number = len(purchases_df["Purchase ID"].value_counts())
+print("Number of Purchases", purchase_number)
+
+# total_revenue
+total_revenue = purchases_df["Price"].sum()
+print("Total Revenue", total_revenue)
+
+
+# ## Gender Demographics
+
+# * Percentage and Count of Male Players
+# 
+# 
+# * Percentage and Count of Female Players
+# 
+# 
+# * Percentage and Count of Other / Non-Disclosed
+# 
+# 
+# 
+
+# In[81]:
+
+
+gender_groups = purchases_df.groupby("Gender")
+gender_groups.head()
+
+# purchases_by_gender_df = gender_df[["SN", "Gender"]]
+# purchases_by_gender_df.head()
+
+
+# 
+# ## Purchasing Analysis (Gender)
+
+# * Run basic calculations to obtain purchase count, avg. purchase price, avg. purchase total per person etc. by gender
+# 
+# 
+# 
+# 
+# * Create a summary data frame to hold the results
+# 
+# 
+# * Optional: give the displayed data cleaner formatting
+# 
+# 
+# * Display the summary data frame
+
+# In[5]:
+
+
+
+
+
+# ## Age Demographics
+
+# * Establish bins for ages
+# 
+# 
+# * Categorize the existing players using the age bins. Hint: use pd.cut()
+# 
+# 
+# * Calculate the numbers and percentages by age group
+# 
+# 
+# * Create a summary data frame to hold the results
+# 
+# 
+# * Optional: round the percentage column to two decimal points
+# 
+# 
+# * Display Age Demographics Table
+# 
+
+# In[6]:
+
+
+
+
+
+# ## Purchasing Analysis (Age)
+
+# * Bin the purchase_data data frame by age
+# 
+# 
+# * Run basic calculations to obtain purchase count, avg. purchase price, avg. purchase total per person etc. in the table below
+# 
+# 
+# * Create a summary data frame to hold the results
+# 
+# 
+# * Optional: give the displayed data cleaner formatting
+# 
+# 
+# * Display the summary data frame
+
+# In[7]:
+
+
+
+
+
+# ## Top Spenders
+
+# * Run basic calculations to obtain the results in the table below
+# 
+# 
+# * Create a summary data frame to hold the results
+# 
+# 
+# * Sort the total purchase value column in descending order
+# 
+# 
+# * Optional: give the displayed data cleaner formatting
+# 
+# 
+# * Display a preview of the summary data frame
+# 
+# 
+
+# In[8]:
+
+
+
+
+
+# ## Most Popular Items
+
+# * Retrieve the Item ID, Item Name, and Item Price columns
+# 
+# 
+# * Group by Item ID and Item Name. Perform calculations to obtain purchase count, average item price, and total purchase value
+# 
+# 
+# * Create a summary data frame to hold the results
+# 
+# 
+# * Sort the purchase count column in descending order
+# 
+# 
+# * Optional: give the displayed data cleaner formatting
+# 
+# 
+# * Display a preview of the summary data frame
+# 
+# 
+
+# In[9]:
+
+
+
+
+
+# ## Most Profitable Items
+
+# * Sort the above table by total purchase value in descending order
+# 
+# 
+# * Optional: give the displayed data cleaner formatting
+# 
+# 
+# * Display a preview of the data frame
+# 
+# 
+
+# In[10]:
+
+
+
+
